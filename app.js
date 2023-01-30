667884c4fbd946223d71d7b19dbc5360d8bdd2ec
Madhev-147 <17mkeecheril@robertclack.co.uk>
2023-01-30 19:47:11 +0000

    w

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1
 
     if (randomNumber === 1) {
-    computerChoice =  'rock'
+    computerChoice =  'roock'
     }
     if (randomNumber === 2) {
     computerChoice =  'paper'   
